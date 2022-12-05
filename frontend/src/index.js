30d270e9bd5cb00076aeef24ac0b056b0a80fe88
bonnieli51 <bonnieli51@gmail.com>
2022-12-04 19:24:21 -0500

    login works. sign up does not

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -35,8 +35,11 @@ const renderApplication = () => {
   );
 }
 
-if (sessionStorage.getItem("X-CSRF-Token") === null) {
-  restoreCSRF().then(renderApplication);
+if (
+  sessionStorage.getItem("currentUser") === null ||
+  sessionStorage.getItem("X-CSRF-Token") === null 
+) {
+  store.dispatch(sessionActions.restoreSession()).then(renderApplication);
 } else {
   renderApplication();
 }
