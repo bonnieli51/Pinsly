ee0c90c0c81576107ccb2a3f9081f4029a608bdf
bonnieli51 <bonnieli51@gmail.com>
2022-12-15 16:39:46 -0500

    finished splash css

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -1,6 +1,7 @@
 import PinIndex from "../Pins/PinIndex/PinIndex";
 import { useSelector } from "react-redux";
 import * as sessionActions from "../../store/session";
+import SignupForm from "../SignupFormModal/SignupForm";
 import "./HomePage.css";
 function HomePage() {
   const sessionUser = useSelector((state) => state.session.user);
@@ -19,17 +20,28 @@ function HomePage() {
     );
   } else {
     return (
-      <>
+      <div >
         <div id="splash-images">
           <img src="https://pinsly-seeds.s3.amazonaws.com/pinsly+images/pinsly+splash+images/splash_2.png"></img>
           <img src="https://pinsly-seeds.s3.amazonaws.com/pinsly+images/pinsly+splash+images/splash_3.png"></img>
           <img src="https://pinsly-seeds.s3.amazonaws.com/pinsly+images/pinsly+splash+images/splash_4.png"></img>
-          <img src="https://pinsly-seeds.s3.amazonaws.com/pinsly+images/pinsly+splash+images/splash_5.png"></img>
+          <img
+            id="splash_img_4"
+            src="https://pinsly-seeds.s3.amazonaws.com/pinsly+images/pinsly+splash+images/splash_1.png"
+          ></img>
+          <div id="splash-sign-up">
+            <SignupForm />
+          </div>
+          <div id="splash-links">
+            <a href="#">About</a>
+            <a href="https://www.linkedin.com/login">LinkedIn</a>
+            <a href="https://github.com/bonnieli51/Pinsly">GitHub</a>
+          </div>
         </div>
         <button class="back-to-top" onClick={goToTop}>
           <i class="fa-solid fa-angle-up"></i>
         </button>
-      </>
+      </div>
     );
   }
 }
