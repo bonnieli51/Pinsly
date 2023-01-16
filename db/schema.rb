ec9a4b62032823ef4149e219d9a12e6353829264
bonnieli51 <bonnieli51@gmail.com>
2023-01-16 13:15:07 -0500

    dropped columns in Pins

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_11_184947) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_16_181342) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -66,10 +66,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_11_184947) do
     t.string "title", null: false
     t.text "description"
     t.bigint "user_id", null: false
-    t.bigint "board_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["board_id"], name: "index_pins_on_board_id"
     t.index ["user_id"], name: "index_pins_on_user_id"
   end
 
@@ -91,6 +89,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_11_184947) do
   add_foreign_key "board_pins", "boards"
   add_foreign_key "board_pins", "pins"
   add_foreign_key "boards", "users"
-  add_foreign_key "pins", "boards"
   add_foreign_key "pins", "users"
 end
