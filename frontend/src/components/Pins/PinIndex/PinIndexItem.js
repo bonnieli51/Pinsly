680e0b9bbd1e7c395eb64ab6502ae7d0bd55e34e
bonnieli51 <bonnieli51@gmail.com>
2023-01-25 13:51:51 -0500

    removed fetchusers

--- frontend/src/components/Pins/PinIndex/PinIndexItem.js
+++ frontend/src/components/Pins/PinIndex/PinIndexItem.js
@@ -1,18 +1,8 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
 import { Link, useHistory } from "react-router-dom";
-import * as usersActions from "../../../store/user";
 import "./PinIndex.css";
 
 function PinIndexItem({ pin }) {
-  const dispatch = useDispatch();
   const history = useHistory();
-  const userId = pin.userId;
-  const user = useSelector(({ users }) => (users[userId] ? users[userId] : {}));
-
-  // useEffect(() => {
-  //   dispatch(usersActions.fetchUser(userId));
-  // }, [dispatch, pin]);
 
   const handleClick = () => {
     history.push(`/pins/${pin.id}`);
@@ -25,8 +15,8 @@ function PinIndexItem({ pin }) {
         <div className="pin-name">{pin.title}</div>
       </div>
       <Link className="pin-user" to={`/${pin.userId}`}>
-        <div id="pin-index-user">{user.username ? user.username[0] : ""}</div>
-        <div>{user.username}</div>
+        <div id="pin-index-user">{pin.username ? pin.username[0] : ""}</div>
+        <div>{pin.username}</div>
       </Link>
     </div>
   );
